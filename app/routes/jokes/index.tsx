d1bea5a5a6c67856b281e489d8d64c9775d3ab34
xovest <79008917+xovest@users.noreply.github.com>
2021-11-29 14:10:09 +0200

    routing

---
+++ app/routes/jokes/index.tsx
@@ -0,0 +1,50 @@
+import type { LoaderFunction } from "remix";
+import { useLoaderData, Link, useCatch } from "remix";
+import type { Joke } from "@prisma/client";
+import { db } from "~/utils/db.server";
+
+type LoaderData = { randomJoke: Joke };
+
+export let loader: LoaderFunction = async () => {
+  let count = await db.joke.count();
+  let randomRowNumber = Math.floor(Math.random() * count);
+  let [randomJoke] = await db.joke.findMany({
+    take: 1,
+    skip: randomRowNumber,
+  });
+  if (!randomJoke) {
+    throw new Response("No random joke found", {
+      status: 404,
+    });
+  }
+  let data: LoaderData = { randomJoke };
+  return data;
+};
+
+export default function JokesIndexRoute() {
+  let data = useLoaderData<LoaderData>();
+
+  return (
+    <div>
+      <p>Here's a random joke:</p>
+      <p>{data.randomJoke.content}</p>
+      <Link to={data.randomJoke.id}>"{data.randomJoke.name}" Permalink</Link>
+    </div>
+  );
+}
+
+export function CatchBoundary() {
+  let caught = useCatch();
+
+  if (caught.status === 404) {
+    return (
+      <div className="error-container">There are no jokes to display.</div>
+    );
+  }
+  throw new Error(`Unexpected caught response with status: ${caught.status}`);
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return <div className="error-container">I did a whoopsies.</div>;
+}
