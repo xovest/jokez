8dda241b4102e1f2aea874c4c005954b19825cef
xovest <79008917+xovest@users.noreply.github.com>
2021-11-29 14:11:28 +0200

    ui modif

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,38 @@
-export default function IndexRoute() {
-  return <div>Hello Index Route</div>;
+import type { LinksFunction, MetaFunction } from "remix";
+import { Link } from "remix";
+import stylesUrl from "../styles/index.css";
+
+export let links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: stylesUrl,
+    },
+  ];
+};
+
+export let meta: MetaFunction = () => {
+  return {
+    title: "Remix: So great, it's funny!",
+    description: "Remix jokes app. Learn Remix and laugh at the same time!",
+  };
+};
+
+export default function Index() {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>
+          Remix <span>Jokes!</span>
+        </h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="jokes">Read Jokes</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
